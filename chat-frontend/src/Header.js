c6fc29267607b56715a840acc64d175bb69f7e1d
La'Tonia Mertica <latoniamertica@gmail.com>
2022-01-01 11:20:28 -0500

    initial commit

---
+++ chat-frontend/src/Header.js
@@ -0,0 +1,21 @@
+const Header = () => {
+  return (
+    <header>
+      <nav class="navbar navNav">
+        <div class="container-fluid navDiv">
+          <a class="navbar-brand" href="#">
+            <img
+              src="/images/placeholderimg.png"
+              alt=""
+              width="44"
+              class="d-inline-block navImg"
+            />
+            <span>&nbsp;&nbsp;NAVBAR-BRAND TEXT</span>
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
